67e764cb9203e17b2baf8c5725dcfe6cd1cf49bf
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-01 21:12:12 +0700

    Hash: add methods has_key(), pair_value()

--- lib/Data/CSel/WrapStruct/Hash.pm
+++ lib/Data/CSel/WrapStruct/Hash.pm
@@ -34,10 +34,18 @@ sub length {
     scalar @{ $_[0][2] };
 }
 
+sub has_key {
+    exists $_[0][0]{$_[1]};
+}
+
+sub pair_value {
+    $_[0][0]{$_[1]};
+}
+
 1;
 # ABSTRACT: Wrap a hashref
 
-=for Pod::Coverage .*
+=for Pod::Coverage ^(parent|children)$
 
 =head1 DESCRIPTION
 
@@ -48,3 +56,30 @@ Some notes:
 =item * The children are hash values, ordered by keys
 
 =back
+
+
+=head1 METHODS
+
+=head2 new($hash, $parent) => obj
+
+=head2 value() => hash
+
+Return the hash.
+
+=head2 length() => int
+
+The number of keys. An empty hash will return 0.
+
+=head2 has_key($key) => bool
+
+Return true if hash has a key with value of C<$key>. Equivalent to:
+
+ exists($hash->{$key})
+
+=head2 pair_value($key) => any
+
+Retrieve a hash pair value. Equivalent to:
+
+ $hash->{$key}
+
+=head2
